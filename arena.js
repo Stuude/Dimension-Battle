61e90763314ff67b27263046e9d767fa8ec2820b
Michel <vilbertmichel9@gmail.com>
2021-10-20 10:03:03 +0200

    fix prblm + commentaires

--- arena.js
+++ arena.js
@@ -1,26 +1,29 @@
 
 // la fonction chooseArena lié au select du html permet l'ensemble des interactions liés à l'arène,elle est composé de variables.
+
 function chooseArena(){
+
 // la variable arena est lié à l'id "map-select"
+
     var arena = document.getElementById("map-select");
+
 // la variable choix reprend arena en lui rajoutant une valeur qui reste à définit
+
     var choix = arena.value;
+
+
+
+
 // la variable div est retrouvée dans le html avec l'id "imgarene" avec le getelementbyId
-var div = document.getElementById("imgarene")    
-
-// A l'aide de switch (choix) nous allons associer des valeurs a arena (voir variable choix)
-switch (choix) {
-
-// La case "Namek" que l'on retrouve dans le html dans les cases option est associé à l'url d'une image afin que celle ci soit chargée dans la div qui a pour id imgarena
-    case "Namek":
-        div.style.backgroundImage = "url('img-arene/namek.PNG')";
-
-
-
 
 var div = document.getElementById("imgarene")
 
+// A l'aide de switch (choix) nous allons associer des valeurs a arena (voir variable choix)
+
 switch (choix) {
+
+    // La case "Namek" que l'on retrouve dans le html dans les cases option est associé à l'url d'une image afin que celle ci soit chargée dans la div qui a pour id imgarena
+
     case "namek":
         div.style.backgroundImage = "url('img-arene/namek.png')";
         break;
