cf5c0875ff66aa140eefb210dd42d78bbb0c0e21
Gregor <failyriece@gmail.com>
2021-10-20 14:05:50 +0200

    Tarte au queches

--- contexte.js
+++ contexte.js
@@ -78,10 +78,18 @@ function ecritContext() {
 
 
 
-
 // FIN CODE AKIN// 
 
+var raclette = [{ name: "Patate", maxLife: 100, actualLife: 100 }, { name: "Haricot", maxLife: 80, actualLife: 80 }];
 
+function LaFete() {
+    var characterUl = document.getElementById('characterUl');
 
+    for (let i = 0; i < raclette.length; i++) {
+        let newLi = document.createElement("LI");
+        let newUl = document.createElement("UL");
+        
+    }
+}
 
 
